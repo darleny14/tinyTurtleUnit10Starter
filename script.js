d6159f6732c8951df11d0d329676982a91e4e48e
Darleny Mendoza <darleny2013madrid@gmail.com>
2017-03-22 12:33:52 +0000

    Three dimensional house

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 }
-
+function triangle(x){
+right(30);
+forward(x);
+right(120);
+forward(x);    
+}
+function dimension(x){
+right(180);
+forward(x);
+left(60);
+forward(x);
+right(-60);
+forward(x);
+left(30);
+forward(x);
+right(-90);
+forward(x);
+    
+}
 // Type your function call below
-
-
+function house(x){
+var x=90
+square(x);
+triangle(x);
+dimension(x);
+}
 stamp();
+house();
